40aa49ffb2fa9140cc032d227a767a5dba3f267a
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2024-03-17 13:26:00 +0530

    user and thera

--- server/routes/therapist/index.js
+++ server/routes/therapist/index.js
@@ -1,15 +1,85 @@
 const router = require("express").Router();
-const ApiError = require("../../utils/ApiError.js")
-const ApiResponse = require('../../utils/ApiResponse.js')
+const ApiError = require("../../utils/ApiError.js");
+const ApiResponse = require("../../utils/ApiResponse.js");
+const User = require("../../db/schemas/users.js");
+const Appointment = require("../../db/schemas/appointment.js");
 
-// user 
+// therapist list
 router.get("/", async (req, res, next) => {
     try {
-        const users = await User.find({role: true}).select("-password").lean()
-        res.json({status: true,message : "Successful", data: users})
+        const users = await User.find({ role: true })
+            .select("-password")
+            .lean();
+        res.json({ status: true, message: "Successful", data: users });
     } catch (err) {
-        next(new ApiError(500, err.message))
+        next(new ApiError(500, err.message));
+    }
+});
+
+// crate appointment
+router.post("appointment", async (req, res, next) => {
+    try {
+        const appointment = await Appointment.create(req.body);
+        res.json({
+            status: true,
+            message: "Appointment created successfully",
+            data: appointment,
+        });
+    } catch (err) {
+        next(new ApiError(500, err.message));
+    }
+});
+
+// read appointment
+router.get("appointment/:id", async (req, res, next) => {
+    try {
+        const appointments = await Appointment.find({
+            _id: req.params.id,
+        }).lean();
+        res.json(
+            new ApiResponse(
+                true,
+                "Appointment fetched successfully",
+                appointments
+            )
+        );
+    } catch (err) {
+        next(new ApiError(200, err.message));
+    }
+});
+
+// appointment list doctors
+app.get("/appointment/doctors/:id", async (req, res, next) => {
+    const doctorId = req.params.id;
+    try {
+        const appointments = await Appointment.find({doctor : doctorId}).lean();
+        res.json(
+            new ApiResponse(
+                true,
+                "Appointments fetched successfully",
+                appointments
+            )
+        );
+    }catch(err){
+        next(new ApiError(200, err.message));
     }
 })
 
-module.exports = router
+// apointments of users
+app.get("/appointment/patients/:id", async (req, res, next) => {
+    const patientId = req.params.id;
+    try {
+        const appointments = await Appointment.find({patient : patientId}).lean();
+        res.json(
+            new ApiResponse(
+                true,
+                "Appointments fetched successfully",
+                appointments
+            )
+        );
+    }catch(err){
+        next(new ApiError(200, err.message));
+    }
+})
+
+module.exports = router;
--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -5,22 +5,42 @@ const router = require("express").Router();
 const ApiResponse = require("../../utils/ApiResponse.js");
 const ApiError = require("../../utils/ApiError.js");
 
-router.get("/", async (req, res) => {
-
-  console.log(req.user, "req.user")
-  const userId = req.user.uid
-  const filteredUsers = await User.find({ uid: userId}).select("-password");
-  console.log(filteredUsers, "filteredUsers")
-  return res.json(new ApiResponse(true, "Users fetched successfully", filteredUsers));
-})
+// router.get("/", async (req, res) => {
+//     console.log(req.user, "req.user");
+//     const userId = req.user.uid;
+//     try {
+//         const users = await User.find({ role: true })
+//             .select("-password")
+//             .lean();
+//         return res.json(
+//             new ApiResponse(true, "Users fetched successfully", users)
+//         );
+//     } catch (err) {
+//         return res.json(new ApiError(200, err.message));
+//     }
+// });
 
 router.get("/:id", async (req, res) => {
-console.log(req.params.id, "req.params.id")
-  const userId = req.params._id
-  const user = await User.findById(userId).select("-password");
-  if (!user) {
-    throw new ApiError(404, "User not found");
-  }
-  return res.json(new ApiResponse(true, "User fetched successfully", user));
-})
+    const userId = req.params._id;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+    return res.json(new ApiResponse(true, "User fetched successfully", user));
+});
+
+router.post("/update", async (req, res) => {
+    const userId = req.user._id;
+    const userUPdate = req.body;
+    const user = await User.findByIdAndUpdate(userId, userUPdate, {
+        new: true,
+    }).select("-password");
+    try {
+        return res.json(
+            new ApiResponse(true, "User updated successfully", user)
+        );
+    } catch (err) {
+        return res.json(new ApiError(200, err.message));
+    }
+});
 module.exports = router;
